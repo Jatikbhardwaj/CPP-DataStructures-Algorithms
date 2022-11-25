9ba4548f12cd6d662ace00f1ea79f1bc6f837136
Jatikbhardwaj <jatikbhardwaj@gmail.com>
2022-11-25 11:46:26 -0800

    Commit

---
+++ CPP/37.Diamond Pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n+n-1;j++){
+     if(j>=n+1-i&&j<=n-1+i)
+        cout<<"*";
+     else
+        cout<<" ";
+    }
+   cout<<endl;
+}
+for(int i=n;i>=1;i--){
+     for(int j=1;j<=n+n-1;j++){
+     if(j>=n+1-i&&j<=n-1+i)
+        cout<<"*";
+     else
+        cout<<" ";
+    }
+   cout<<endl;
+    }
+}
